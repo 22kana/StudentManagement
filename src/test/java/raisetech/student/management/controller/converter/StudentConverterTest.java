9f3a5e5a17f647d4f474a907e6d1b2838d2f7d35
22kana <081922kana@gmail.com>
2025-11-14 13:58:21 +0900

    Converterのテストコード修正

--- src/test/java/raisetech/student/management/controller/converter/StudentConverterTest.java
+++ src/test/java/raisetech/student/management/controller/converter/StudentConverterTest.java
@@ -1,12 +1,10 @@
 package raisetech.student.management.controller.converter;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 
-import java.util.ArrayList;
+import java.time.LocalDateTime;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import raisetech.student.management.data.Student;
 import raisetech.student.management.data.StudentCourse;
@@ -14,36 +12,69 @@ import raisetech.student.management.domain.StudentDetail;
 
 class StudentConverterTest {
 
-  private StudentConverter converter;
+  private StudentConverter sut;
+
+  @BeforeEach
+  void before(){
+    sut = new StudentConverter();
+  }
 
   @Test
-  void 受講生に紐づく受講生コース情報をマッピングできていること(){
+  void 受講生のリストと受講生コース情報のリストを渡して受講生詳細のリストが作成できること(){
 
-    converter = new StudentConverter();
+    Student student = createStudent();
 
+    StudentCourse studentCourse = new StudentCourse();
+    studentCourse.setId("1");
+    studentCourse.setStudentId("1");
+    studentCourse.setCourseName("Javaコース");
+    studentCourse.setCourseStartAt(LocalDateTime.now());
+    studentCourse.setCourseEndAt(LocalDateTime.now().plusYears(1));
+
+    List<Student> studentList = List.of(student);
+    List<StudentCourse> studentCourseList = List.of(studentCourse);
+
+    List<StudentDetail> actual = sut.convertStudentDetails(studentList, studentCourseList);
+
+    assertThat(actual.get(0).getStudent()).isEqualTo(student);
+    assertThat(actual.get(0).getStudentCourseList()).isEqualTo(studentCourseList);
+  }
+
+  @Test
+  void 受講生のリストと受講生コース情報のリストを渡した時に紐づかない受講生コース情報は除外されること(){
+
+    Student student = createStudent();
+
+    StudentCourse studentCourse = new StudentCourse();
+    studentCourse.setId("1");
+    studentCourse.setStudentId("2");
+    studentCourse.setCourseName("Javaコース");
+    studentCourse.setCourseStartAt(LocalDateTime.now());
+    studentCourse.setCourseEndAt(LocalDateTime.now().plusYears(1));
+
+    List<Student> studentList = List.of(student);
+    List<StudentCourse> studentCourseList = List.of(studentCourse);
+
+    List<StudentDetail> actual = sut.convertStudentDetails(studentList, studentCourseList);
+
+    assertThat(actual.get(0).getStudent()).isEqualTo(student);
+    assertThat(actual.get(0).getStudentCourseList()).isEmpty();
+  }
+
+  private static Student createStudent() {
     Student student = new Student();
     student.setId("1");
     student.setName("田中太郎");
-    List<Student> studentList = new ArrayList<>();
-    studentList.add(student);
-
-    StudentCourse studentCourse = new StudentCourse();
-    studentCourse.setStudentId("1");
-    studentCourse.setCourseName("Javaコース");
-    List<StudentCourse> studentCourseList = new ArrayList<>();
-    studentCourseList.add(studentCourse);
-
-    List<StudentDetail> studentDetails = new ArrayList<>();
-
-    List<StudentDetail> expected = new ArrayList<>();
-    expected.add(new StudentDetail(student, studentCourseList));
-    List<StudentDetail> actual = converter.convertStudentDetails(studentList, studentCourseList);
-
-    assertThat(actual).hasSize(1);
-    assertThat(actual.get(0).getStudent().getName()).isEqualTo("田中太郎");
-    assertThat(actual.get(0).getStudentCourseList())
-        .extracting(StudentCourse::getCourseName)
-        .containsOnly("Javaコース");
+    student.setKanaName("タナカタロウ");
+    student.setNickname("タロウ");
+    student.setEmail("taro@example.com");
+    student.setArea("大阪");
+    student.setAge(27);
+    student.setSex("男");
+    student.setRemark("");
+    student.setDeleted(false);
+    return student;
 
   }
+
 }
